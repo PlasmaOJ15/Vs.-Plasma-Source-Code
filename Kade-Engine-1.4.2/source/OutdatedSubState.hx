4f4c262ccb98bb9e32f1fcb8c103ddf2235125e1
PlasmaOJ15 <orlandorodriquezjr@gmail.com>
2021-07-28 23:40:21 -0700

    PlasmaOJ15 Here!

---
+++ Kade-Engine-1.4.2/source/OutdatedSubState.hx
@@ -0,0 +1,45 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxSubState;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import lime.app.Application;
+
+class OutdatedSubState extends MusicBeatState
+{
+	public static var leftState:Bool = false;
+
+	public static var needVer:String = "IDFK LOL";
+
+	override function create()
+	{
+		super.create();
+		var bg:FlxSprite = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
+		add(bg);
+		var txt:FlxText = new FlxText(0, 0, FlxG.width,
+			"Greetings Player!\n"
+			+ "Thank you for downloading the Vs. PlasmaOJ15 mod (vs. Plasma for short)\n"
+			+ "If you wish, pressing enter will take you to my youtube channel, if not, you can press escape to continue to the mod"
+			+ "\n I hope you enjoy my mod! (and dont beat me that bad please) - PlasmaOJ15",
+			32);
+		txt.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, CENTER);
+		txt.screenCenter();
+		add(txt);
+	}
+
+	override function update(elapsed:Float)
+	{
+		if (controls.ACCEPT)
+		{
+			FlxG.openURL("https://www.youtube.com/channel/UC3fXZ3vnuVV2RSke84WTu5Q");
+		}
+		if (controls.BACK)
+		{
+			leftState = true;
+			FlxG.switchState(new MainMenuState());
+		}
+		super.update(elapsed);
+	}
+}
